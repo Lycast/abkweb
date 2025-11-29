7ed0ad68a41f5b265aa6bd77d8976c57e9db5199
Lycast <anthonybrenon49@gmail.com>
2025-11-29 17:40:15 +0100

    modify netlify.toml

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Assurer que le script Gradle est exécutable
-chmod +x gradlew
-# Définir la version de Java à 17
-export JAVA_VERSION=17
-# Lancer la construction Kotlin/Gradle (la commande la plus simple)
-./gradlew :webTest:jsJar
-# Le script doit réussir pour que Netlify continue
